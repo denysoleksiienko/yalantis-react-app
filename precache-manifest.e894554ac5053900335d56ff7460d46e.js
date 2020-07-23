5130574488e1c8017aad2be2ddc45e460330eb55
Denis Alekseenko <den.effective@gmail.com>
2020-07-23 12:10:41 +0300

    Updates

---
+++ precache-manifest.e894554ac5053900335d56ff7460d46e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8b78d483bc834454fee7053352ef16",
+    "url": "/denysoleksiienko/yalantis-react-app.git/index.html"
+  },
+  {
+    "revision": "300f16b87b4c773ade02",
+    "url": "/denysoleksiienko/yalantis-react-app.git/static/css/main.d1d30399.chunk.css"
+  },
+  {
+    "revision": "b42100bf0f3478523bab",
+    "url": "/denysoleksiienko/yalantis-react-app.git/static/js/2.d2a53e89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/denysoleksiienko/yalantis-react-app.git/static/js/2.d2a53e89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "300f16b87b4c773ade02",
+    "url": "/denysoleksiienko/yalantis-react-app.git/static/js/main.0f8f09b1.chunk.js"
+  },
+  {
+    "revision": "e50d829c5b02632ce00b",
+    "url": "/denysoleksiienko/yalantis-react-app.git/static/js/runtime-main.8bba7c70.js"
+  }
+]);
